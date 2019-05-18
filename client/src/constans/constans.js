622ccc6b469bf6dd895cd05ae44d86b615702efb
Olga Kanishcheva <kanishcheva@phystech.edu>
2019-05-18 21:24:20 +0300

    save

--- client/src/constans/constans.js
+++ client/src/constans/constans.js
@@ -2,7 +2,8 @@ const constans = {
   SORT_BOOKS: 'SORT_BOOKS',
   ADD_BOOK: 'ADD_BOOK',
   REMOVE_BOOK: 'REMOVE_BOOK',
-  EDIT_BOOK: 'EDIT_BOOK'
+  EDIT_BOOK: 'EDIT_BOOK',
+  DELETE_BOOK: 'DELETE_BOOK'
 }
 
 export default constans
